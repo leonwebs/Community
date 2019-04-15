763cf9a570549057b797d0457ee8e2bc6c884e16
taramclancy <taramclancy@gmail.com>
2019-04-15 08:15:27 -0600

    Use regions module from pysal

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 ##
 ![gocode-logo](https://cloud.githubusercontent.com/assets/100216/12792545/96727a8e-ca69-11e5-9b9a-cddfa80d1c4b.png)
 --
-Update me with info about your application!
+This is an entry into the data analysis track.  Colorado's
+redistricting commission is entrusted with the authority to draw
+Congressional district boundaries.  Among its considerations must be 
+>... communities of interest, including ethnic, cultural, economic,
+>trade area, geographic, and demographic factors, shall be preserved
+>within a single district wherever possible. " 
+[Redistricting provisions](https://www.colorado.gov/pacific/cga-redistrict/constitutional-provisions)
 
-Click [here](https://github.com/GoCodeColorado/CommunityCluster/projects/1) to review your project task list created by the Go Code Colorado team, or navigate to it from your github code repository by clicking the "Projects" tab.
+Preservation of communities of interest is not quantitatively defined.
+This code provides a way for interest groups, the business desicion
+makers, to quantitatively evaluate how well the boundaries provided by
+the commission preserve communities and decide whether a challenge is
+warranted.
--- redistrictingold.py
+++ redistrictingold.py
@@ -10,11 +10,13 @@ Created on Thu Apr  4 17:20:07 2019
 
 #import requests
 import os 
-import pysal as ps
+#import urllib2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pysal as ps
 import geopandas as gpd
+from sklearn import cluster
 import region
 
 
@@ -54,15 +56,54 @@ agecat = [i for i in tractdat.columns if 'age' in i and i not in badcol]
 educationcat = [i for i in tractdat.columns if 'gr' in i and i not in badcol]
 educationcat.extend(['enrolled', 'n_enrolled'])
 
+tractdatchoice = tractdat[racecat]
+
 ndist = 7
 distpop = tractdat.population.sum() / ndist 
+
+
+#this chunk just clusters without regard to contiguity
+#km1 = cluster.KMeans(n_clusters = ndist)
+#clustkm1 = km1.fit(tractdat.values)
+#
+#f, ax = plt.subplots(1, figsize=(9, 9))
+#
+#tractdat.assign(cl=clustkm1.labels_).plot(column = 'cl', categorical = True, legend= True, ax=ax)
+#
+#ax.set_axis_off()
+#
+#plt.show()
+
+
+
 
 # Set the seed for reproducibility
 np.random.seed(1234)
 
+
+## max p regionalization is in psal.region but this chooses ndist for you
+
+#ps.region.maxp needs shapefiles to say which tracts are contiguous
+#w = ps.queen_from_shapefile('geofile/geofile.shp', idVariable = 'geonum')
+
+#z = tractdatchoice.values
+#print("Beginning maxp regionalization ...")
+#maxp = ps.region.Maxp(w, z, distpop*.99, tractdat.population, initial=300)
+#print("... done.")
+#
+#lbls = pd.Series(maxp.area2region).reindex(tractdat['geonum'])
+#
+#f, ax = plt.subplots(1, figsize=(9, 9))
+#tractdat.assign(cl=lbls.values).plot(column='cl', categorical=True, legend=True, linewidth=0.1, edgecolor='white', ax=ax)
+#ax.set_axis_off()
+#plt.show()
+
+
+# to get other algorithms find the region package that ports clusterpy to Py3
+
 pregazp = region.p_regions.azp.AZP()
 print("Beginning regionalization ...")
-pregazp.fit_from_geodataframe(countydat, racecat, ndist, contiguity = "queen")
+#pregazp.fit_from_geodataframe(countydat, racecat, 7, contiguity = "queen")
 print("... done.")
 
 f, ax = plt.subplots(1, figsize=(9,9))
@@ -77,7 +118,6 @@ ax.set_axis_off()
 f.show()
 
 ctylabeled.to_file(filename + '_lab.geojson', driver='GeoJSON')
-
 
 
 #lots of help from http://darribas.org/gds_scipy16/ipynb_md/07_spatial_clustering.html
